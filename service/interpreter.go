f6d388edc5f4730010d7d4eb6a20d5b80019658f
Viktor Naychev <vitonaychev@gmail.com>
2024-05-30 16:02:15 +0300

    initial interpreter implementation

---
+++ service/interpreter.go
@@ -0,0 +1,47 @@
+package service
+
+import "strconv"
+
+func Interpret(tokens []Token) int {
+	result := parseNumberToken(tokens[0].(*NumberToken))
+
+	opPtr := 0
+	numPtr := 1
+
+	tokens = tokens[1:]
+	for len(tokens) != 0 {
+		numToken := tokens[numPtr].(*NumberToken)
+		number := parseNumberToken(numToken)
+
+		opToken := tokens[opPtr].(*OperandToken)
+		result = executeOperandToken(opToken, result, number)
+
+		tokens = tokens[2:]
+	}
+
+	return result
+}
+
+func parseNumberToken(token *NumberToken) int {
+	str := token.GetToken().(string)
+	num, _ := strconv.Atoi(str)
+
+	return num
+}
+
+func executeOperandToken(token *OperandToken, left, right int) int {
+	var result int
+
+	switch token.GetToken().(string) {
+	case "plus":
+		result = left + right
+	case "minus":
+		result = left - right
+	case "multiplied by":
+		result = left * right
+	case "divided by":
+		result = left / right
+	}
+
+	return result
+}
