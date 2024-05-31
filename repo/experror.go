aab44d7429d3cdd9f2cc21c73c540607ef385fd0
Viktor Naychev <vitonaychev@gmail.com>
2024-05-31 15:52:35 +0300

    in-memory repo implementation

---
+++ repo/experror.go
@@ -0,0 +1,44 @@
+package repo
+
+import (
+	"sync"
+
+	"github.com/VitoNaychev/eval-web-service/service"
+)
+
+type InMemoryExprErrorRepository struct {
+	exprErrors map[string]*service.ExpressionError
+	mu         sync.Mutex
+}
+
+func NewInMemoryErrorRepository() *InMemoryExprErrorRepository {
+	return &InMemoryExprErrorRepository{
+		exprErrors: make(map[string]*service.ExpressionError),
+	}
+}
+
+func (repo *InMemoryExprErrorRepository) Increment(exprError *service.ExpressionError) error {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
+	if existing, exists := repo.exprErrors[exprError.Expression]; exists {
+		existing.Frequency++
+	} else {
+		exprError.Frequency = 1
+		repo.exprErrors[exprError.Expression] = exprError
+	}
+
+	return nil
+}
+
+func (repo *InMemoryExprErrorRepository) GetAll() ([]service.ExpressionError, error) {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
+	var allErrors []service.ExpressionError
+	for _, exprError := range repo.exprErrors {
+		allErrors = append(allErrors, *exprError)
+	}
+
+	return allErrors, nil
+}
