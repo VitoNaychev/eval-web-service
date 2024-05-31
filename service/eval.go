098ee7d89d8aaaf309f8e87ba9f0c162ca0d4279
Viktor Naychev <vitonaychev@gmail.com>
2024-05-31 14:28:33 +0300

    clear up logic implementation a bit by moving type definitions

--- service/eval.go
+++ service/eval.go
@@ -2,59 +2,7 @@ package service
 
 import (
 	"errors"
-	"fmt"
 )
-
-type EvalServiceError struct {
-	msg string
-}
-
-func NewEvalServiceError(msg string) error {
-	return &EvalServiceError{
-		msg: msg,
-	}
-}
-
-func (e *EvalServiceError) Error() string {
-	return e.msg
-}
-
-var (
-	ErrNonMathQuestion      = NewEvalServiceError("non-math question")
-	ErrUnsupportedOperation = NewEvalServiceError("unsupported operation")
-	ErrInvalidSyntax        = NewEvalServiceError("invalid syntax")
-)
-
-type UnsupportedInterpreterError struct {
-	msg string
-}
-
-func NewUnsupportedInterpreterError(msg string) error {
-	return &UnsupportedInterpreterError{
-		msg: fmt.Sprintf("unsupported interpreter error: %s", msg),
-	}
-}
-
-func (u *UnsupportedInterpreterError) Error() string {
-	return u.msg
-}
-
-func (u *UnsupportedInterpreterError) As(target interface{}) bool {
-	if evalServiceError, ok := target.(**EvalServiceError); ok {
-		*evalServiceError = &EvalServiceError{msg: u.msg}
-		return true
-	}
-	return false
-}
-
-type Interpreter interface {
-	Validate(string) (bool, error)
-	Execute(string) (int, error)
-}
-
-type ErrorRepository interface {
-	Increment(*ExpressionError) error
-}
 
 type EvalService struct {
 	interp    Interpreter
